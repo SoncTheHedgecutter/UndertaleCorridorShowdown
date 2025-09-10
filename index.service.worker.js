a0312a4cfb5b26716dbfbc0a71ec263367c487e5
SoncTheHedgecutter <somebodywasheir@gmail.com>
2025-09-09 22:05:43 -0400

    please

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1757469089|2418626659';
+const CACHE_VERSION = '1757469935|3265000891';
 /** @type {string} */
 const CACHE_PREFIX = 'Last-corridor-sh-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
