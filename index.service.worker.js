6e654f02a123845f327b690bc3aed5ea25562f4d
SoncTheHedgecutter <somebodywasheir@gmail.com>
2025-09-09 22:09:03 -0400

    Yolo

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1757469935|3265000891';
+const CACHE_VERSION = '1757470135|3464883948';
 /** @type {string} */
 const CACHE_PREFIX = 'Last-corridor-sh-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
