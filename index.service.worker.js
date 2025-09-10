643aea66b964da701b240ad50a7ea562697e3afc
SoncTheHedgecutter <somebodywasheir@gmail.com>
2025-09-09 22:31:35 -0400

    BandWidth?

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1757470135|3464883948';
+const CACHE_VERSION = '1757471484|19087491';
 /** @type {string} */
 const CACHE_PREFIX = 'Last-corridor-sh-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
